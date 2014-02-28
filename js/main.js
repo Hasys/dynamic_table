d524e45f9c5785f9ccfb8b2281f31446b62bb714
Kirill (hasys) Gaevskii <hasysin@gmail.com>
2014-02-28 23:00:28 +0100

    EventBinder|Unbinder added.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,55 @@
 window.onload = function(){
 	window.dynamic_table_factory('table_one');
 	window.dynamic_table_factory('table_two');
-}
+};
+
+
 
 var dynamic_table_factory = function(id){
+
+	var body = document.getElementsByTagName('body')[0];
 	var table = document.getElementById(id);
-	
-	table.onclick = function(){
-		alert('Hello from ' + id);
+	var hcells = table.getElementsByTagName('th');
+
+	var bindEvent = function(element, event, func) {
+		if( element.addEventListener ) {
+			element.addEventListener( event, func, false );
+		} else if ( element.attachEvent ) {
+			element.attachEvent( 'on'+event, func );
+		}
+	};
+
+	var unbindEvent = function(element, event, func) {
+		if( element.removeEventListener ) {
+			element.removeEventListener( event, func, false );
+		} else if ( element.detachEvent ) {
+			element.detachEvent( 'on'+event, func );
+		}
+	};
+
+	var headerMouseDownEvent = function() {
+		this.classList.add('down');
+		for(i = 0; i < hcells.length; i++)	{
+			hcells[i].classList.add('cursor-move');
+		}
+		body.classList.add('text-not-selectable');
+		body.classList.add('cursor-move');
+		bindEvent( body, 'mouseup', bodyMouseUpEvent );
+	};
+
+	var bodyMouseUpEvent = function() {
+		console.log('Event! ' + id);
+		for(i = 0; i < hcells.length; i++)	{
+			hcells[i].classList.remove('down');
+			hcells[i].classList.remove('cursor-move');
+			body.classList.remove('cursor-move');
+		}
+		unbindEvent( body, 'mouseup', bodyMouseUpEvent );
+		body.classList.remove('text-not-selectable');
+	};
+
+	for(i = 0; i < hcells.length; i++)	{
+		hcells[i].classList.add('cursor-pointer');
+		bindEvent( hcells[i], 'mousedown', headerMouseDownEvent );
 	}
-}
+};
