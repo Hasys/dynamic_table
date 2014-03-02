b36e239b11686862733ff8e11cf5e847a66a96e5
Kirill (hasys) Gaevskii <hasysin@gmail.com>
2014-03-03 00:24:44 +0100

    Basic column moving.

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@ var dynamic_table_factory = function(id){
 
 	var body = document.getElementsByTagName('body')[0];
 	var table = document.getElementById(id);
-	var hcells = table.getElementsByTagName('th');
+	var hcells = table.getElementsByTagName('tr')[0].getElementsByTagName('th');
+	var draggingHeaderID = -1;
+	var droppingID = -1;
 
 	this.bindEvent = function(element, event, func) {
 		if( element.addEventListener ) {
@@ -28,42 +30,104 @@ var dynamic_table_factory = function(id){
 	};
 
 	var bodyMouseMoveEvent = function( event ) {
-		druggedDiv.style.left = event.x+'px';
-		druggedDiv.style.top = event.y+'px';
+		draggableDiv.style.left = (event.x +5)+'px'; //+5 for good looking and dragging
+		draggableDiv.style.top = (event.y +5)+'px'; //+5 for good looking and dragging
+	}
+
+	var headMouseOverEvent = function() {
+		this.classList.add('can-to-drop');
+	}
+	var headMouseOutEvent = function() {
+		this.classList.remove('can-to-drop');	
 	}
 
 	var headerMouseDownEvent = function( event ) {
 		this.classList.add('down');
-		var cell_width = 100;
 		for(i = 0; i < hcells.length; i++)	{
 			hcells[i].classList.add('cursor-move');
+			if( this == hcells[i] ) {
+				draggingHeaderID = i;
+			} else {
+				bindEvent( hcells[i], 'mouseover', headMouseOverEvent );
+				bindEvent( hcells[i], 'mouseout', headMouseOutEvent );
+			}
 		}
+
 		body.classList.add('text-not-selectable');
 		body.classList.add('cursor-move');
-		
-		druggedDiv.style.display = 'block';
+
+		draggableDiv.style.display = 'block';
 		var border = 2; //Two times width of borders in px
-		druggedDiv.style.width = ( this.offsetWidth - border )+'px';
-		druggedDiv.style.height = ( this.offsetHeight - border )+'px';
-		druggedDiv.style.left = event.x+'px';
-		druggedDiv.style.top = event.y+'px';
-		druggedDiv.innerHTML = this.innerHTML;
+		draggableDiv.style.width = ( this.offsetWidth - border )+'px';
+		draggableDiv.style.height = ( this.offsetHeight - border )+'px';
+		draggableDiv.style.left = event.x+'px';
+		draggableDiv.style.top = event.y+'px';
+		draggableDiv.innerHTML = this.innerHTML;
 
 		bindEvent( body, 'mouseup', bodyMouseUpEvent );
 		bindEvent( body, 'mousemove', bodyMouseMoveEvent, true );
 	};
 
+	var moveColumn = function() {
+		if(droppingID == -1 || draggingHeaderID == -1) 
+			return;
+		var rowsTmp = table.getElementsByTagName('tr');
+		var rows = new Array();
+		for (i=0;i<rowsTmp.length;i++){
+			if (rowsTmp.item(i).nodeType==1)
+				rows.push(rowsTmp.item(i));
+			
+		}
+
+		for(j = 0; j<rows.length; j++) {
+			var tmpArr = new Array();
+			var row = (j==0)? rows[j].getElementsByTagName('th') : rows[j].getElementsByTagName('td');
+			for (i=0;i<row.length;i++){
+				if (row.item(i).nodeType==1)
+					tmpArr.push(row.item(i));
+			}
+
+			rows[j].innerHTML = '';
+
+			for( i = 0; i<droppingID; i++ ) {
+				if(i == draggingHeaderID) continue;
+				rows[j].appendChild(tmpArr[i]);
+			}
+			if(tmpArr[draggingHeaderID] !== undefined)
+			rows[j].appendChild(tmpArr[draggingHeaderID]);
+			for( i=droppingID; i<tmpArr.length; i++){
+				if(i == draggingHeaderID) continue;
+				rows[j].appendChild(tmpArr[i]);
+			}
+		}
+
+		hcells = table.getElementsByTagName('tr')[0].getElementsByTagName('th');
+	};
+
 	var bodyMouseUpEvent = function() {
+		//Styles section
 		for(i = 0; i < hcells.length; i++)	{
+			if (hcells[i].classList.contains('can-to-drop') ) {
+				droppingID = i;
+			}
 			hcells[i].classList.remove('down');
 			hcells[i].classList.remove('cursor-move');
+			hcells[i].classList.remove('can-to-drop');
+			unbindEvent( hcells[i], 'mouseover', headMouseOverEvent );
+			unbindEvent( hcells[i], 'mouseout', headMouseOutEvent )
 		}
+		body.classList.remove('text-not-selectable');
+		body.classList.remove('cursor-move');
+		
+		draggableDiv.style.display = 'none';
+
+		//Event section
 		unbindEvent( body, 'mouseup', bodyMouseUpEvent );
 		unbindEvent( body, 'mousemove', bodyMouseMoveEvent );
 
-		body.classList.remove('text-not-selectable');
-		body.classList.remove('cursor-move');
-		druggedDiv.style.display = 'none';
+		moveColumn();
+
+		draggingHeaderID = -1;
 	};
 
 	for(i = 0; i < hcells.length; i++)	{
@@ -71,10 +135,10 @@ var dynamic_table_factory = function(id){
 		bindEvent( hcells[i], 'mousedown', headerMouseDownEvent );
 	}
 
-	if(!this.druggedDiv) {
-		this.druggedDiv = document.createElement('div');
-		druggedDiv.classList.add('drugged-div');
-		druggedDiv.style.display = 'none';
-		body.appendChild(druggedDiv);
+	if(!this.draggableDiv) {
+		this.draggableDiv = document.createElement('div');
+		draggableDiv.classList.add('draggable-div');
+		draggableDiv.style.display = 'none';
+		body.appendChild(draggableDiv);
 	}
 };
